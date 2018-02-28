5448fc0ab38cae865dbaad4a5bd550939673de62
siddu509 <gsk.chakravarthi@gmail.com>
2018-02-27 20:19:24 -0600

    Added method in FirstContollerList class related to update,delete,save and etc. Changed name for "ValidatingData.html" to "SalesPersonList.html" Made few changes to SalesPersonList.html page Added methods to SalesPersonDaoImpl class

--- InvoiceWebApp/src/main/java/com/nitco/DaoImpl/SalesPersonDaoImpl.java
+++ InvoiceWebApp/src/main/java/com/nitco/DaoImpl/SalesPersonDaoImpl.java
@@ -16,21 +16,34 @@ public class SalesPersonDaoImpl {
 
 	@Autowired
 	private SalesPersonDao salesPersonDaoRepo;
-	
+
 	private Date date;
-	
-	private List<SalesPerson> salesPerson= Arrays.asList(
-			new SalesPerson("AARON", "COLLINS", "SOFTHQ", 168,date, date, "AARCOL12312017"),
-			new SalesPerson("JUSTIN", "DANIEL", "NHRG", 168,date, date, "JUSDAN12312017"));
-	
-	public List<SalesPerson> getSalesPersons(){
+
+	private List<SalesPerson> salesPerson = Arrays.asList(
+			new SalesPerson("AARON", "COLLINS", "SOFTHQ", 168, date, date, "AARCOL12312017"),
+			new SalesPerson("JUSTIN", "DANIEL", "NHRG", 168, date, date, "JUSDAN12312017"));
+
+	public List<SalesPerson> getSalesPersonsFromRepo() {
 		List<SalesPerson> listOfSalesPerson = new ArrayList<>();
 		salesPersonDaoRepo.findAll().forEach(listOfSalesPerson::add);
 		return listOfSalesPerson;
 	}
-	
-	public void addSalesPerson() {
+
+	public void addSalesPersonToRepo() {
 		salesPersonDaoRepo.save(salesPerson);
+	}
+
+	public SalesPerson getSalesPersonFromRepo(String invoiceNumber) {
+		return salesPersonDaoRepo.findOne(invoiceNumber);
+
+	}
+	
+	public void deleteSalesPersonInRepo(String invoiceNumber) {
+		salesPersonDaoRepo.delete(invoiceNumber);
+	}
+	
+	public void updateSalesPersonInRepo(SalesPerson salesPersonRepo) {
+		salesPersonDaoRepo.save(salesPersonRepo);
 	}
 
 }
--- InvoiceWebApp/src/main/java/com/nitco/controller/FirstControllerClass.java
+++ InvoiceWebApp/src/main/java/com/nitco/controller/FirstControllerClass.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -12,30 +13,48 @@ import org.springframework.web.servlet.ModelAndView;
 import com.nitco.DaoImpl.SalesPersonDaoImpl;
 import com.nitco.model.SalesPerson;
 
-
-
 @Controller
 public class FirstControllerClass {
 
 	@Autowired
 	SalesPersonDaoImpl salesPersonDaoImpl;
-
-	
 			
 	@RequestMapping(value = "/AddSalesPerson", method = RequestMethod.GET)
 	public ModelAndView addSalesPerson(Model model) {
-		salesPersonDaoImpl.addSalesPerson();
-		return new ModelAndView("hello","helloString","Hello from the Thymeleaf");
+		salesPersonDaoImpl.addSalesPersonToRepo();
+		return new ModelAndView("hello","helloString","Added the SalesPersons to the Derby DB");
 	}
 
-	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String homePage(Model model) {
-		List<SalesPerson> salesPersonList = salesPersonDaoImpl.getSalesPersons();
+	@RequestMapping(value = "/salesPersons", method = RequestMethod.GET)
+	public String getListOfSalesPersons(Model model) {
+		List<SalesPerson> salesPersonList = salesPersonDaoImpl.getSalesPersonsFromRepo();
 		for(SalesPerson s:salesPersonList) {
 			System.out.println(s.getFirstName());
 		}
 		model.addAttribute(salesPersonList);
-		return "ValidatingData";
-
+		return "SalesPersonList";
+	}
+	
+	@RequestMapping(value="/",method=RequestMethod.GET)
+	public String homePage() {
+		return "welcome";
+	}
+	
+	@RequestMapping(value="/SalesPersons/{invoiceNumber}")
+	public String getSalesPerson(@PathVariable("invoiceNumber") String invoiceNumber, Model model) {
+		//SalesPerson person = salesPersonDaoImpl.getSalesPerson(invoiceNumber);
+		model.addAttribute((SalesPerson)salesPersonDaoImpl.getSalesPersonFromRepo(invoiceNumber));
+		return "salesPersonSingle";
+		
+	}
+	
+	@RequestMapping(value="/delete/{invoiceNumber}")
+	public void deleteSalesPerson(@PathVariable("invoiceNumber") String invoiceNumber) {
+		salesPersonDaoImpl.deleteSalesPersonInRepo(invoiceNumber);
+	}
+	
+	@RequestMapping(value="/update/{salesPerson}")
+	public void updateSalesPerson(@PathVariable("salesPerson") SalesPerson salesPerson) {
+		salesPersonDaoImpl.updateSalesPersonInRepo(salesPerson);
 	}
 }
