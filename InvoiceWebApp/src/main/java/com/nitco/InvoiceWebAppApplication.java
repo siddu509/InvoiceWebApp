914d88d4a6caa80ead5113d2c76afcbba49be00e
siddu509 <gsk.chakravarthi@gmail.com>
2018-03-06 13:18:14 -0600

    updating to recent version

--- InvoiceWebApp/src/main/java/com/nitco/InvoiceWebAppApplication.java
+++ InvoiceWebApp/src/main/java/com/nitco/InvoiceWebAppApplication.java
@@ -2,8 +2,10 @@ package com.nitco;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages={"com.nitco.controller","com.nitco.dao","com.nitco.DaoImpl","com.nitco.model"})
 public class InvoiceWebAppApplication {
 
 	public static void main(String[] args) {
